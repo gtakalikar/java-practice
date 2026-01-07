68c62a550da28f4bea96248c7e1af366b65e7692
Your Name <user@example.com>
2026-01-07 21:58:18 +0530

    variable declaration and then initialization

---
+++ var1.java
@@ -0,0 +1,32 @@
+class Var1 
+{
+	public static void main(String[] args) 
+	{
+		String name ;
+		char grade ;
+		boolean isAlive ;
+		short salary ;
+		long phoneno ;
+		float cgpa ;
+		byte age ;
+		String degree ;
+		
+		name = "Gayatri" ;
+		grade = 'A' ;
+		isAlive = true ;
+		salary = 5000 ;
+		phoneno = 8010346471l ;
+		cgpa = 9.64f ;
+		age = 22 ;
+		degree = "BE" ;
+		
+		System.out.println(name);
+		System.out.println(grade);
+		System.out.println(isAlive);
+		System.out.println(salary);
+		System.out.println(phoneno);
+		System.out.println(cgpa);
+		System.out.println(age);
+		System.out.println(degree);
+	}
+}
