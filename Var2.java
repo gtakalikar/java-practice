cc1da9d155e17d342ac141abae23caed4677320b
Gayatri Takalikar <gayatritakalikar@gmail.com>
2026-01-12 19:11:50 +0530

    declare variable and store value simultaneously

---
+++ Var2.java
@@ -0,0 +1,17 @@
+class Var2 
+{
+	public static void main(String[] args) 
+	{
+		String name = "Gayatri" ;
+		char grade = 'A' ;
+		boolean isAlive =  true ;
+		byte age = 22 ;
+		float percent = 84f ;
+		
+		System.out.println(name);
+		System.out.println(grade);
+		System.out.println(isAlive);
+		System.out.println(age);
+		System.out.println(percent);
+	}
+}
