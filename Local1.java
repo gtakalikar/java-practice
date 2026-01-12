99c27827aab94b795f1efa4583eb625c03e85e40
Gayatri Takalikar <gayatritakalikar@gmail.com>
2026-01-12 19:25:02 +0530

    local variable inside one block and its compile time errors

---
+++ Local1.java
@@ -0,0 +1,15 @@
+class  Local1
+{
+	public static void main(String[] args) 
+	{
+		int a = 100;
+		System.out.println(a); // 100
+		//char a = 'a'; //Compile Time Error
+	}
+	// System.out.println(a); // Compile Time Error
+    {
+		char a = 'a';
+		System.out.println(a);// a
+	
+	}
+}
